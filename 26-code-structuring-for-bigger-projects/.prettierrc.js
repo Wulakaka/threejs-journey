aade8ad1dedec65dc2ce537fcc3c75d639490285
张伟峰 <zhang.weifeng@tslsmart.com>
2024-02-18 17:47:00 +0800

    format(26): 增加 prettier 配置

---
+++ 26-code-structuring-for-bigger-projects/.prettierrc.js
@@ -0,0 +1,25 @@
+export default {
+  // trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  // arrowParens: 'always',
+  // bracketSpacing: true,
+  // printWidth: 120,
+  // endOfLine: 'auto',
+  // jsxBracketSameLine: false,
+  // jsxSingleQuote: false,
+  // quoteProps: 'as-needed',
+  // useTabs: false,
+  // vueIndentScriptAndStyle: false,
+  // htmlWhitespaceSensitivity: 'css',
+  // embeddedLanguageFormatting: 'auto',
+  // overrides: [
+  //   {
+  //     files: '*.json',
+  //     options: {
+  //       tabWidth: 2,
+  //     },
+  //   },
+  // ],
+}

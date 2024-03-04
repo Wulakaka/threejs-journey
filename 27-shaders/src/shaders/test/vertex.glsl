a5018111ec3780e713d72aeddafadc54a240250d
张伟峰 <wulakaka@live.com>
2024-03-04 22:46:15 +0800

    feat(28-shaders): 增加 attribute 传递

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -2,6 +2,8 @@ uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
+attribute float aRandom;
+
 // 不同顶点对应的值不同
 attribute vec3 position;
 
@@ -9,6 +11,7 @@ void main() {
   // 从右往左乘
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+  modelPosition.z += aRandom * 0.1;
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 

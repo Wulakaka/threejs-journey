2335205fca92813207332d946bad9bc835931b3f
张伟峰 <wulakaka@live.com>
2024-03-04 23:27:05 +0800

    feat(27-shaders): 增加旗子及明暗变化

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -2,9 +2,15 @@
 precision mediump float;
 
 uniform vec3 uColor;
+uniform sampler2D uTexture;
 
 varying float vRandom;
+varying vec2 vUv;
+varying float vElevation;
 
 void main() {
-  gl_FragColor = vec4(uColor, 1.0);
+  vec4 textureColor = texture2D(uTexture, vUv);
+  // 明暗变化
+  textureColor.rgb *= vElevation * 2.0 + 0.5;
+  gl_FragColor = textureColor;
 }
--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -4,7 +4,13 @@ uniform mat4 modelMatrix;
 
 attribute float aRandom;
 
+attribute vec2 uv;
+
 varying float vRandom;
+
+varying vec2 vUv;
+
+varying float vElevation;
 
 // 不同顶点对应的值不同
 attribute vec3 position;
@@ -18,12 +24,18 @@ void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
   // modelPosition.z += aRandom * 0.1;
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+  modelPosition.z += elevation;
+
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
 
   vRandom = aRandom;
+
+  vUv = uv;
+
+  vElevation = elevation;
 }

554347c79abd16e10f55cd026272656e2466f74a
张伟峰 <zhang.weifeng@tslsmart.com>
2024-03-05 10:12:35 +0800

    feat(28-shader-pattern): 增加颜色混合

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -3,7 +3,7 @@
 varying vec2 vUv;
 float random(vec2 st)
 {
-    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
 
 vec2 rotate(vec2 uv, float rotation, vec2 mid)
@@ -19,12 +19,12 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid)
 //
 vec2 fade(vec2 t)
 {
-    return t*t*t*(t*(t*6.0-15.0)+10.0);
+    return t * t * t * (t * (t * 6.0 - 15.0) + 10.0);
 }
 
 vec4 permute(vec4 x)
 {
-    return mod(((x*34.0)+1.0)*x, 289.0);
+    return mod(((x * 34.0) + 1.0) * x, 289.0);
 }
 
 float cnoise(vec2 P)
@@ -41,10 +41,10 @@ float cnoise(vec2 P)
     vec4 gy = abs(gx) - 0.5;
     vec4 tx = floor(gx + 0.5);
     gx = gx - tx;
-    vec2 g00 = vec2(gx.x,gy.x);
-    vec2 g10 = vec2(gx.y,gy.y);
-    vec2 g01 = vec2(gx.z,gy.z);
-    vec2 g11 = vec2(gx.w,gy.w);
+    vec2 g00 = vec2(gx.x, gy.x);
+    vec2 g10 = vec2(gx.y, gy.y);
+    vec2 g01 = vec2(gx.z, gy.z);
+    vec2 g11 = vec2(gx.w, gy.w);
     vec4 norm = 1.79284291400159 - 0.85373472095314 * vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
     g00 *= norm.x;
     g01 *= norm.y;
@@ -91,7 +91,8 @@ void main()
 //    barX = step(0.8, barX);
 //    float barY = mod(vUv.y * 10.0, 1.0);
 //    barY = step(0.8, barY);
-//    float bar = barX + barY;
+//    float strength = barX + barY;
+//    strength = clamp(strength, 0.0, 1.0);
 //    Pattern 12
 //    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
 //    strength *= step(0.8,mod(vUv.y * 10.0, 1.0));
@@ -102,11 +103,13 @@ void main()
 //    float barX = step(0.4, mod(vUv.x * 10.0, 1.0)) * step(0.8,mod(vUv.y * 10.0, 1.0));
 //    float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4,mod(vUv.y * 10.0, 1.0));
 //    float strength = barX + barY;
+//    strength = clamp(strength, 0.0, 1.0);
 //    Pattern 15
 //    在 mod 函数中 -0.2 是为了让条纹的向左移动 0.2，也就是更晚到达边界
 //    float barX = step(0.4, mod(vUv.x * 10.0 - 0.2, 1.0)) * step(0.8, mod(vUv.y * 10.0, 1.0));
 //    float barY = step(0.8, mod(vUv.x * 10.0, 1.0)) * step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
 //    float strength = barX + barY;
+//    strength = clamp(strength, 0.0, 1.0);
 //    Pattern 16
 //    float strength = abs(vUv.x - 0.5);
 //    Pattern 17
@@ -215,6 +218,10 @@ void main()
 //    Pattern 49
 //    float strength = sin(cnoise(vUv * 10.0) * 20.0);
 //    Pattern 50
-    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
-gl_FragColor = vec4(vec3(strength), 1.0);
+//    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    gl_FragColor = vec4(mixedColor, 1.0);
 }

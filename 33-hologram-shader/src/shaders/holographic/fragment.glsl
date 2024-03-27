7c215ad60bad3d69abc81e96e8e915f1d72731d8
张伟峰 <wulakaka@live.com>
2024-03-27 22:21:44 +0800

    feat(33-hologram-shader): 增加 fresnel

--- 33-hologram-shader/src/shaders/holographic/fragment.glsl
+++ 33-hologram-shader/src/shaders/holographic/fragment.glsl
@@ -1,16 +1,28 @@
 uniform float uTime;
 
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main() {
     float stripes = vPosition.y;
+
+    vec3 normal = normalize(vNormal);
 
     //    形成条纹
     stripes = fract((stripes - uTime * 0.02) * 20.0);
     //    变化更锐利
     stripes = pow(stripes, 2.0);
 
-    gl_FragColor = vec4(1.0, 1.0, 1.0, stripes);
+    // Fresnel
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    float fresnel = dot(viewDirection, normal) + 1.0;
+    fresnel = pow(fresnel, 2.0);
+
+    // Holographic
+    float holographic = stripes * fresnel;
+    holographic += 1.25 * fresnel;
+
+    gl_FragColor = vec4(1.0, 1.0, 1.0, holographic);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>

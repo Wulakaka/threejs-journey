d439ba322cf84420e4c887b80116dabf5f4129e9
张伟峰 <wulakaka@live.com>
2024-03-27 22:46:53 +0800

    feat(33-hologram-shader): 显示背面

--- 33-hologram-shader/src/shaders/holographic/fragment.glsl
+++ 33-hologram-shader/src/shaders/holographic/fragment.glsl
@@ -4,9 +4,22 @@ varying vec3 vPosition;
 varying vec3 vNormal;
 
 void main() {
+    //    float edge = 0.8;
+    //
+    //    float x = vPosition.x * 20.0;
+    //    x = fract(x);
+    //    x = step( edge,x);
+    //    float y = vPosition.y * 20.0;
+    //    y = fract(y);
+    //    y = step(edge, y);
+    //    float point = x * y;
+
     float stripes = vPosition.y;
 
     vec3 normal = normalize(vNormal);
+    if (!gl_FrontFacing) {
+        normal *= -1.0;
+    }
 
     //    形成条纹
     stripes = fract((stripes - uTime * 0.02) * 20.0);

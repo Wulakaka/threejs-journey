1f3b229485c92fc1fef7b667dc0c805db596d4ba
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-02 09:58:03 +0800

    feat(34-fireworks-shader): 增加粒子

--- 34-fireworks-shaders/src/script.js
+++ 34-fireworks-shaders/src/script.js
@@ -1,12 +1,14 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import fireworksVertexShader from './shaders/fireworks/vertex.glsl'
+import fireworksFragmentShader from './shaders/fireworks/fragment.glsl'
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI({ width: 340 })
+const gui = new GUI({width: 340})
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -21,23 +23,22 @@ const textureLoader = new THREE.TextureLoader()
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -56,34 +57,64 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
- * Test
+ * Fireworks
  */
-const test = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(test)
+
+// 由于最终是通过点击触发，所以通过函数创建
+//   count: 粒子数量
+//   position: 粒子中心位置
+const createFirework = (count, position) => {
+
+  const positionsArray = new Float32Array(count * 3)
+
+  for (let i = 0; i < count; i++) {
+    const i3 = i * 3
+    positionsArray[i3 + 0] = Math.random() - 0.5
+    positionsArray[i3 + 1] = Math.random() - 0.5
+    positionsArray[i3 + 2] = Math.random() - 0.5
+
+  }
+
+
+//   Geometry
+//   粒子特效使用BufferGeometry
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute('position', new THREE.BufferAttribute(positionsArray, 3))
+
+  // material
+  const material = new THREE.ShaderMaterial({
+    vertexShader: fireworksVertexShader,
+    fragmentShader: fireworksFragmentShader,
+  })
+
+  // Points
+  const firework = new THREE.Points(geometry, material)
+  firework.position.copy(position)
+  scene.add(firework)
+}
+
+createFirework(500, new THREE.Vector3())
+
 
 /**
  * Animate
  */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+const tick = () => {
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()

60ac8bdec30d05c136daa8e50a89c3a36d7bc70c
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-09 09:33:45 +0800

    feat(35-lights-shading): 增加环境光

---
+++ 35-lights-shading-shaders/src/shaders/includes/ambientLight.glsl
@@ -0,0 +1,4 @@
+vec3 ambientLight(vec3 light, float lightIntensity) {
+    return light * lightIntensity;
+}
+
--- 35-lights-shading-shaders/src/shaders/shading/fragment.glsl
+++ 35-lights-shading-shaders/src/shaders/shading/fragment.glsl
@@ -1,8 +1,14 @@
 uniform vec3 uColor;
 
+#include ../includes/ambientLight.glsl
 void main()
 {
     vec3 color = uColor;
+
+    vec3 light = vec3(0.0);
+    light += ambientLight(vec3(1.0), 0.5);
+
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);

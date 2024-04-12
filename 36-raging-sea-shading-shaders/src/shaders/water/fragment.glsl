00addc1d65ce25b3e56794b589b87a40a09276f8
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-12 09:53:37 +0800

    feat(36-raging-sea-shading-shaders): 增加光的相关文件

--- 36-raging-sea-shading-shaders/src/shaders/water/fragment.glsl
+++ 36-raging-sea-shading-shaders/src/shaders/water/fragment.glsl
@@ -7,9 +7,14 @@ varying float vElevation;
 
 void main()
 {
+    // Base color
     float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    // 使用smoothstep函数，让两边的颜色更重
+    mixStrength = smoothstep(0.0, 1.0, mixStrength);
     vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
-    
+
+    // Final color
     gl_FragColor = vec4(color, 1.0);
+    #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }

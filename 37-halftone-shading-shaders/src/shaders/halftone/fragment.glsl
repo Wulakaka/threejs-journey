2e9516342c91a1774b61b44612594a8a547d2147
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-24 16:39:39 +0800

    feat(37-halftone-shaders): 增加环境光

--- 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
+++ 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
@@ -3,11 +3,22 @@ uniform vec3 uColor;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+#include ../includes/ambientLight.glsl
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = uColor;
+
+    // Lights
+    vec3 light = vec3(0.0);
+    light += ambientLight(
+        vec3(1.0), // 白光
+        1.0
+    );
+
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);

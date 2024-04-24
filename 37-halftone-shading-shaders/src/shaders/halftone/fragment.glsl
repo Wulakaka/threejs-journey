3807ce3b43e8c8bd740d65996ccb1fb30dfb856d
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-24 17:36:48 +0800

    feat(37-halftone-shaders): 将 halftone 组织为单独方法

--- 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
+++ 37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
@@ -7,6 +7,29 @@ varying vec3 vPosition;
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
 
+vec3 halftone(
+    vec3 color,
+    float repetitions,
+    vec3 direction,
+    float low,
+    float high,
+    vec3 pointColor,
+    vec3 normal
+) {
+    // 光源与法线的点积，如果方向和法线相同，点积为1，如果方向和法线垂直，点积为0
+    float intensity = dot(normal, direction);
+    // remap the intensity to the range 0.0 - 1.0
+    intensity = smoothstep(low, high, intensity);
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv = mod(uv * repetitions, 1.0);
+
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point);
+    return mix(color, pointColor, point);
+}
+
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
@@ -20,38 +43,30 @@ void main()
         1.0
     );
     light += directionalLight(
-    vec3(1.0, 1.0, 1.0), // Light color
-    1.0,                 // Light intensity
-    normal,              // Normal
-    vec3(1.0, 1.0, 0.0), // Light position
-    viewDirection,       // View direction
-    1.0                  // Specular power
+        vec3(1.0, 1.0, 1.0), // Light color
+        1.0,                 // Light intensity
+        normal,              // Normal
+        vec3(1.0, 1.0, 0.0), // Light position
+        viewDirection,       // View direction
+        1.0                  // Specular power
     );
 
     color *= light;
 
     // Halftone
-    float repetitions = 30.0;
-    vec3 direction = vec3(0.0, -1.0, 0.0);
-    float low = - 0.8;
-    float high = 1.5;
-    vec3 pointColor = vec3(1.0, 0.0, 0.0);
-
-    vec2 uv = gl_FragCoord.xy / uResolution.y;
-    uv = mod(uv * repetitions, 1.0);
-
-    // 光源与法线的点积，如果方向和法线相同，点积为1，如果方向和法线垂直，点积为0
-    float intensity = dot(normal, direction);
-    // remap the intensity to the range 0.0 - 1.0
-    intensity = smoothstep(low, high, intensity);
-
-    float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5 * intensity, point);
-    pointColor = mix(color,pointColor, point);
+    color = halftone(
+        color,                 // Input color
+        50.0,                  // Repetitions
+        vec3(0.0, - 1.0, 0.0), // Direction
+        - 0.8,                 // Low
+        1.5,                   // High
+        vec3(1.0, 0.0, 0.0),   // Point color
+        normal                 // Normal
+    );
 
 
     // Final color
-    gl_FragColor = vec4(pointColor, 1.0);
+    gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }

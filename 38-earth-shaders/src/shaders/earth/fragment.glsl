1d5d7adf8322b4cb7e411f0a41e6f5f83e04a823
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-26 10:32:56 +0800

    feat(38-earth-shaders): 加载地球纹理

--- 38-earth-shaders/src/shaders/earth/fragment.glsl
+++ 38-earth-shaders/src/shaders/earth/fragment.glsl
@@ -2,14 +2,22 @@ varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularTexture;
+
 void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
     vec3 color = vec3(vUv, 1.0);
 
+    // texture
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+
     // Final color
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(nightColor, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }

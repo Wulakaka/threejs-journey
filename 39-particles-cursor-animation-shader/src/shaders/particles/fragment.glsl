77b851f51f675f96d70af13a2a5c2e64fb2188d1
张伟峰 <zhang.weifeng@tslsmart.com>
2024-06-13 09:44:49 +0800

    feat(39-particles-cursor-animation-shader): 绘制 particle 为锐利的圆形

--- 39-particles-cursor-animation-shader/src/shaders/particles/fragment.glsl
+++ 39-particles-cursor-animation-shader/src/shaders/particles/fragment.glsl
@@ -1,5 +1,13 @@
 void main()
 {
+    vec2 uv = gl_PointCoord;
+
+    // 绘制锐利的圆形
+    float distanceToCenter = distance(uv, vec2(0.5));
+    if (distanceToCenter > 0.5) {
+        discard;
+    }
+
     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>

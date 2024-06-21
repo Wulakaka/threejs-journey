c3d9736ea2cbf661c473fc6475d2ca3b78180a0e
张伟峰 <zhang.weifeng@tslsmart.com>
2024-06-21 10:41:32 +0800

    feat(41-gpgpu-flow-field-particles-shaders): 增加 base geometry 和 gpgpu

--- 41-gpgpu-flow-field-particles-shaders/src/script.js
+++ 41-gpgpu-flow-field-particles-shaders/src/script.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import { GPUComputationRenderer } from "three/addons/misc/GPUComputationRenderer.js";
 import GUI from "lil-gui";
 import particlesVertexShader from "./shaders/particles/vertex.glsl";
 import particlesFragmentShader from "./shaders/particles/fragment.glsl";
@@ -87,12 +88,32 @@ debugObject.clearColor = "#29191f";
 renderer.setClearColor(debugObject.clearColor);
 
 /**
+ * Base geometry
+ */
+const baseGeometry = {};
+baseGeometry.instance = new THREE.SphereGeometry(3);
+// 记录顶点数量
+baseGeometry.count = baseGeometry.instance.attributes.position.count;
+
+/**
+ * GPU Compute
+ */
+// Setup
+const gpgpu = {};
+gpgpu.size = Math.ceil(Math.sqrt(baseGeometry.count));
+gpgpu.computation = new GPUComputationRenderer(
+  gpgpu.size,
+  gpgpu.size,
+  renderer,
+);
+console.log(gpgpu.computation);
+
+/**
  * Particles
  */
 const particles = {};
 
 // Geometry
-particles.geometry = new THREE.SphereGeometry(3);
 
 // Material
 particles.material = new THREE.ShaderMaterial({
@@ -110,7 +131,7 @@ particles.material = new THREE.ShaderMaterial({
 });
 
 // Points
-particles.points = new THREE.Points(particles.geometry, particles.material);
+particles.points = new THREE.Points(baseGeometry.instance, particles.material);
 scene.add(particles.points);
 
 /**

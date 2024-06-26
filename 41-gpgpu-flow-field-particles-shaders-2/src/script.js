0bec52d1cdaa0f8f1caa42a26416236b0a2ef09b
张伟峰 <zhang.weifeng@tslsmart.com>
2024-06-26 10:45:46 +0800

    feat(41-gpgpu-flow-field-particles-shaders-2): 增加更多对 flowField 的控制

--- 41-gpgpu-flow-field-particles-shaders-2/src/script.js
+++ 41-gpgpu-flow-field-particles-shaders-2/src/script.js
@@ -129,11 +129,19 @@ gpgpu.particlesVariable = gpgpu.computation.addVariable(
   gpgpuParticlesShader,
   baseParticlesTexture,
 );
+
+// Uniforms
 gpgpu.particlesVariable.material.uniforms.uTime = new THREE.Uniform(0);
 gpgpu.particlesVariable.material.uniforms.uDeltaTime = new THREE.Uniform(0);
 gpgpu.particlesVariable.material.uniforms.uBase = new THREE.Uniform(
   baseParticlesTexture,
 );
+gpgpu.particlesVariable.material.uniforms.uFlowFieldStrength =
+  new THREE.Uniform(2);
+gpgpu.particlesVariable.material.uniforms.uFlowFieldFrequency =
+  new THREE.Uniform(0.5);
+gpgpu.particlesVariable.material.uniforms.uFlowFieldInfluence =
+  new THREE.Uniform(0.5);
 
 // 设置依赖
 gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [
@@ -212,7 +220,24 @@ gui
   .max(1)
   .step(0.001)
   .name("uSize");
-
+gui
+  .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldStrength, "value")
+  .min(0)
+  .max(10)
+  .step(0.1)
+  .name("uFlowFieldStrength");
+gui
+  .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldFrequency, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uFlowFieldFrequency");
+gui
+  .add(gpgpu.particlesVariable.material.uniforms.uFlowFieldInfluence, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uFlowFieldInfluence");
 /**
  * Animate
  */
--- 41-gpgpu-flow-field-particles-shaders-2/src/shaders/gpgpu/particles.glsl
+++ 41-gpgpu-flow-field-particles-shaders-2/src/shaders/gpgpu/particles.glsl
@@ -1,11 +1,15 @@
 uniform float uTime;
 uniform float uDeltaTime;
 uniform sampler2D uBase;
+uniform float uFlowFieldStrength;
+uniform float uFlowFieldFrequency;
+uniform float uFlowFieldInfluence;
 
 #include ../includes/simplexNoise4d.glsl
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
-
+    // 降低了时间的速度
+    float time = uTime * 0.2;
     vec4 particles = texture(uParticles, uv);
     vec4 base = texture(uBase, uv);
 
@@ -14,14 +18,23 @@ void main() {
         particles.xyz = base.xyz;
 
     } else {
+
+        // 根据时间的不同，每个点移动的速度会有变化
+        float strength = simplexNoise4d(vec4(base.xyz, time + 1.0));
+        // 让某些点不发生位置变化
+        float influence = (uFlowFieldInfluence - 0.5) * -2.0;
+        strength = smoothstep(influence, 1.0, strength);
+
         vec3 flowField = vec3(
-            simplexNoise4d(vec4(particles.xyz + 1.0, uTime)),
-            simplexNoise4d(vec4(particles.xyz + 2.0, uTime)),
-            simplexNoise4d(vec4(particles.xyz + 3.0, uTime))
+            simplexNoise4d(vec4(particles.xyz * uFlowFieldFrequency + 1.0, time)),
+            simplexNoise4d(vec4(particles.xyz * uFlowFieldFrequency + 2.0, time)),
+            simplexNoise4d(vec4(particles.xyz * uFlowFieldFrequency + 3.0, time))
         );
 
         flowField = normalize(flowField);
-        particles.xyz += flowField * uDeltaTime * 0.5;
+        particles.xyz += flowField * strength * uDeltaTime * uFlowFieldStrength;
+
+        // Decay 生命周期消亡
         particles.a += uDeltaTime * 0.3;
     }
 

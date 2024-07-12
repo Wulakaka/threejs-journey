81c1579f698ce5509fcae2d4cc03d65360622eb7
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-12 16:44:19 +0800

    doc(45-post-processing): 增加 displacement pass

--- 45-post-processing/src/script.js
+++ 45-post-processing/src/script.js
@@ -240,6 +240,46 @@ gui
 
 effectComposer.addPass(tintPass);
 
+// displacement
+const displacementShader = {
+  uniforms: {
+    tDiffuse: {
+      value: null,
+    },
+    uTime: {
+      value: null,
+    },
+  },
+  vertexShader: `
+    varying vec2 vUv;
+    
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      
+      vUv = uv;
+    }
+  `,
+  fragmentShader: `
+    uniform sampler2D tDiffuse;
+    uniform float uTime;
+    
+    varying vec2 vUv;
+    
+    void main() {
+      vec2 newUv = vec2(
+        vUv.x,
+        vUv.y + sin(vUv.x * 10.0 + uTime) * 0.1
+      );
+      vec4 color = texture(tDiffuse, newUv);
+      gl_FragColor = color;
+    }
+  `,
+};
+const displacementPass = new ShaderPass(displacementShader);
+displacementPass.material.uniforms.uTime.value = 0;
+effectComposer.addPass(displacementPass);
+
+// 伽马校正
 const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
 effectComposer.addPass(gammaCorrectionPass);
 
@@ -258,6 +298,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update passes
+  displacementPass.material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();

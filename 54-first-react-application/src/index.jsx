7f0c4c259bb9789a7d4bea0202629ea931c50aa5
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-18 17:43:25 +0800

    feat(54-first-react-application): 增加内容

--- 54-first-react-application/src/index.jsx
+++ 54-first-react-application/src/index.jsx
@@ -1,8 +1,16 @@
-import './style.css'
-import { createRoot } from 'react-dom/client'
+import "./style.css";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
+
+const todo = "react";
 
 root.render(
-    <h1>Hello React</h1>
-)
+  <div>
+    <h1 className="cute-paragraph">Hello {todo}</h1>
+    <p>
+      Some <br />
+      content {Math.random()}
+    </p>
+  </div>,
+);

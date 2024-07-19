1b17e8a6afb6e5fe6b0f41d72f35509ee5ef369d
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-19 14:06:43 +0800

    feat(54-first-react-application): 增加 useRef 示例

--- 54-first-react-application/src/Clicker.jsx
+++ 54-first-react-application/src/Clicker.jsx
@@ -1,16 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Clicker({ keyName, color, increment }) {
-  // 需要在 useState 中直接获取，否则通过在 useEffect 中获取之后再设置会导致多次渲染
+  // 需要在 useState 中直接获取当前值，否则通过在 useEffect 中获取之后再设置会导致多次渲染
   const [count, setCount] = useState(
     parseInt(localStorage.getItem(keyName) ?? 0),
   );
+
+  const buttonRef = useRef();
 
   useEffect(() => {
     localStorage.setItem(keyName, count);
   }, [count]);
 
   useEffect(() => {
+    buttonRef.current.style.backgroundColor = "papayawhip";
+    buttonRef.current.style.color = "salmon";
     return () => {
       localStorage.removeItem(keyName);
     };
@@ -24,7 +28,9 @@ export default function Clicker({ keyName, color, increment }) {
   return (
     <div>
       <div style={{ color }}>Click counts: {count}</div>
-      <button onClick={buttonClick}>Click me</button>
+      <button ref={buttonRef} onClick={buttonClick}>
+        Click me
+      </button>
     </div>
   );
 }

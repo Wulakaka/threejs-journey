466fe324fc953b2b97ce5751569f4913a796be14
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-19 10:34:06 +0800

    feat(54-first-react-application): 持久化数据

--- 54-first-react-application/src/Clicker.jsx
+++ 54-first-react-application/src/Clicker.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Clicker() {
-  const [count, setCount] = useState(0);
+  // 需要在 useState 中直接获取，否则通过在 useEffect 中获取之后再设置会导致多次渲染
+  const [count, setCount] = useState(
+    parseInt(localStorage.getItem("count")) ?? 0,
+  );
+
+  console.log("render");
+
+  useEffect(() => {
+    localStorage.setItem("count", count);
+  }, [count]);
 
   const buttonClick = () => {
     // setCount((value) => value + 1);

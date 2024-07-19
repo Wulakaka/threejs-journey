d5176dddfb56774d942ca46e322c494a444a8385
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-19 10:47:50 +0800

    feat(54-first-react-application): 添加条件逻辑和卸载逻辑

--- 54-first-react-application/src/App.jsx
+++ 54-first-react-application/src/App.jsx
@@ -1,9 +1,18 @@
 import Clicker from "./Clicker.jsx";
+import { useState } from "react";
 
 export default function App() {
+  const [hasClicker, setHasClicker] = useState(true);
+
+  const toggleClicker = () => {
+    setHasClicker(!hasClicker);
+  };
   return (
     <>
-      <Clicker></Clicker>
+      <button onClick={toggleClicker}>
+        {hasClicker ? "Hide" : "Show"} Clicker
+      </button>
+      {hasClicker && <Clicker />}
     </>
   );
 }
--- 54-first-react-application/src/Clicker.jsx
+++ 54-first-react-application/src/Clicker.jsx
@@ -3,14 +3,18 @@ import { useState, useEffect } from "react";
 export default function Clicker() {
   // 需要在 useState 中直接获取，否则通过在 useEffect 中获取之后再设置会导致多次渲染
   const [count, setCount] = useState(
-    parseInt(localStorage.getItem("count")) ?? 0,
+    parseInt(localStorage.getItem("count") ?? 0),
   );
-
-  console.log("render");
 
   useEffect(() => {
     localStorage.setItem("count", count);
   }, [count]);
+
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem("count");
+    };
+  }, []);
 
   const buttonClick = () => {
     // setCount((value) => value + 1);

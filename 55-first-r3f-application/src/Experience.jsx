2f368a8cf2bcd7dc382df23bd09d6f384c24423e
张伟峰 <wulakaka@live.com>
2024-07-19 20:58:54 +0800

    feat(55-first-r3f-application): 添加 Experience.jsx

---
+++ 55-first-r3f-application/src/Experience.jsx
@@ -0,0 +1,10 @@
+export default function Experience() {
+  return (
+    <>
+      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshBasicMaterial color="mediumpurple" wireframe />
+      </mesh>
+    </>
+  );
+}

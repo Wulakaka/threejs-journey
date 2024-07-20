8d668e5ae05862bf8c3bbbd2b9915ba67312bf47
zhangweifeng <zhang.weifeng@tslsmart.com>
2024-07-20 11:19:46 +0800

    feat(55-first-r3f-application): add sphere and plane

--- 55-first-r3f-application/src/Experience.jsx
+++ 55-first-r3f-application/src/Experience.jsx
@@ -3,7 +3,15 @@ export default function Experience() {
     <>
       <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
         <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" wireframe />
+        <meshBasicMaterial color="mediumpurple" />
+      </mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" />
+      </mesh>
+      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
       </mesh>
     </>
   );

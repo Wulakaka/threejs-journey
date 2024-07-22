157faa4dcf90d15b11d5e0c50ea709440b41682c
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-22 15:55:36 +0800

    feat(55-first-r3f-application): 设置 antialias toneMapping outputColorSpace

--- 55-first-r3f-application/src/index.jsx
+++ 55-first-r3f-application/src/index.jsx
@@ -2,19 +2,28 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
+import {
+  ACESFilmicToneMapping,
+  LinearSRGBColorSpace,
+  SRGBColorSpace,
+} from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
   <>
     <Canvas
-      orthographic
+      gl={{
+        antialias: true,
+        toneMapping: ACESFilmicToneMapping,
+        outputColorSpace: SRGBColorSpace,
+      }}
+      orthographic={false}
       camera={{
         fov: 45,
         near: 0.1,
         far: 200,
         position: [3, 2, 6],
-        zoom: 100,
       }}
     >
       <Experience />

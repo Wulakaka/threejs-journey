ea5d86e600faa8854c4319a4d505b139e366914e
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-22 15:43:20 +0800

    feat(55-first-r3f-application): 设置 camera

--- 55-first-r3f-application/src/index.jsx
+++ 55-first-r3f-application/src/index.jsx
@@ -7,7 +7,16 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
   <>
-    <Canvas>
+    <Canvas
+      orthographic
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [3, 2, 6],
+        zoom: 100,
+      }}
+    >
       <Experience />
     </Canvas>
   </>,

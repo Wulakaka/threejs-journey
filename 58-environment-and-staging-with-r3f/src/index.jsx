2895f0234fda333606fd99606b372b1c2234ed63
张伟峰 <zhang.weifeng@tslsmart.com>
2024-07-26 09:46:24 +0800

    feat(58-environment-and-staging-with-r3f): 添加 Canvas onCreated 钩子

--- 58-environment-and-staging-with-r3f/src/index.jsx
+++ 58-environment-and-staging-with-r3f/src/index.jsx
@@ -5,6 +5,10 @@ import Experience from "./Experience.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
+function created(state) {
+  console.log(state);
+}
+
 root.render(
   <Canvas
     shadows
@@ -14,6 +18,7 @@ root.render(
       far: 200,
       position: [-4, 3, 6],
     }}
+    onCreated={created}
   >
     <color args={["ivory"]} attach="background"></color>
     <Experience />

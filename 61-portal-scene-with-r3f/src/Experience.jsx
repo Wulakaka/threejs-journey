2ca802599a029d70e420c8eca570346fc41a5d36
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-02 11:07:28 +0800

    feat(61-portal-scene-with-r3f): 调整背景色，加载模型

--- 61-portal-scene-with-r3f/src/Experience.jsx
+++ 61-portal-scene-with-r3f/src/Experience.jsx
@@ -1,8 +1,14 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
 export default function Experience() {
+  const model = useGLTF("./model/portal.glb");
+
+  console.log(model);
+
   return (
     <>
+      <color args={["#030202"]} attach="background" />
+
       <OrbitControls makeDefault />
 
       <mesh scale={1.5}>

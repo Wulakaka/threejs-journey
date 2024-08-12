e05e60e936083f7fc4493727ce1b56b04b98fcc0
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-12 10:35:57 +0800

    feat(62-mouse-event-with-r3f): 增加 perf

--- 62-mouse-events-with-r3f/src/index.jsx
+++ 62-mouse-events-with-r3f/src/index.jsx
@@ -2,6 +2,7 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
+import { Perf } from "r3f-perf";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -14,6 +15,7 @@ root.render(
       position: [-4, 3, 6],
     }}
   >
+    <Perf position="top-left" />
     <Experience />
   </Canvas>,
 );

54c9837d06cfca99c82995b1cab60c0073838a6d
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-12 11:39:34 +0800

    feat(62-mouse-event-with-r3f): 处理遮挡

--- 62-mouse-events-with-r3f/src/Experience.jsx
+++ 62-mouse-events-with-r3f/src/Experience.jsx
@@ -9,7 +9,8 @@ export default function Experience() {
     cube.current.rotation.y += delta * 0.2;
   });
 
-  const eventHandler = () => {
+  const eventHandler = (event) => {
+    console.log(event);
     cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
   };
 
@@ -20,7 +21,7 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh position-x={-2}>
+      <mesh position-x={-2} onClick={(event) => event.stopPropagation()}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>

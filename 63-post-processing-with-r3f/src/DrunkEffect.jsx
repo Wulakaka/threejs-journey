16912afedc5ce58b376767870dfa31a8643e3db1
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-13 17:54:26 +0800

    feat(63-post-processing-with-r3f): DrunkEffect.jsx 中增加 UV 和颜色的调整

--- 63-post-processing-with-r3f/src/DrunkEffect.jsx
+++ 63-post-processing-with-r3f/src/DrunkEffect.jsx
@@ -1,8 +1,13 @@
 import { Effect } from "postprocessing";
 
 const fragmentShader = /* glsl */ `
+  void mainUv(inout vec2 uv){
+    uv.y += sin(uv.x * 10.0) * 0.1;
+  }
   void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-    outputColor = vec4(uv, 1.0, 1.0);
+    vec4 color = inputColor;
+    color.rgb *= vec3(0.8, 1.0, 0.5);
+    outputColor = color;
   }
 `;
 

3cbdcf50ced8333af9d16e5e706561b275e0c490
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-14 10:36:52 +0800

    feat(64-fun-and-simple-portfolio-with-r3f): 增加 PresentationControls limits

--- 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -16,7 +16,12 @@ export default function Experience() {
 
       <Environment files="the_sky_is_on_fire_2k.hdr" />
 
-      <PresentationControls global>
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+      >
         <Float rotationIntensity={0.4}>
           <primitive object={computer.scene} position-y={-1.2} />
         </Float>

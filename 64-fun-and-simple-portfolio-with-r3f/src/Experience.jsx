cb3241899e007ac7c87f994e7be0f22551ee31e5
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-14 10:21:24 +0800

    feat(64-fun-and-simple-portfolio-with-r3f): 添加模型；添加 Environment

--- 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,14 +1,21 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, Float, OrbitControls, useGLTF } from "@react-three/drei";
 
 export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf",
+  );
+
   return (
     <>
+      <color args={["#241a1a"]} attach="background" />
+
+      <Environment files="the_sky_is_on_fire_2k.hdr" />
+
       <OrbitControls makeDefault />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Float rotationIntensity={0.4}>
+        <primitive object={computer.scene} position-y={-1.2} />
+      </Float>
     </>
   );
 }

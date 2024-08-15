86dc574930bffb98c0e9248eba1127bc5af86163
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-15 16:29:22 +0800

    feat(65-physics-with-r3f): 设置物体移动

--- 65-physics-with-r3f/src/Experience.jsx
+++ 65-physics-with-r3f/src/Experience.jsx
@@ -7,6 +7,8 @@ import {
   RigidBody,
 } from "@react-three/rapier";
 import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Experience() {
   const cube = useRef();
@@ -33,6 +35,22 @@ export default function Experience() {
       z: Math.random() - 0.5,
     });
   };
+
+  const twister = useRef();
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+
+    const eulerRotation = new THREE.Euler(0, time * 3, 0);
+    const quaternionRotation = new THREE.Quaternion();
+    quaternionRotation.setFromEuler(eulerRotation);
+    twister.current.setNextKinematicRotation(quaternionRotation);
+
+    const angle = time * 0.5;
+    const x = Math.cos(angle) * 2;
+    const z = Math.sin(angle) * 2;
+    twister.current.setNextKinematicTranslation({ x, y: -0.8, z });
+  });
 
   return (
     <>
@@ -70,6 +88,18 @@ export default function Experience() {
           <mesh receiveShadow position-y={-1.25}>
             <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody
+          ref={twister}
+          position={[0, -0.8, 0]}
+          friction={0}
+          type="kinematicPosition"
+        >
+          <mesh castShadow scale={[0.4, 0.4, 3]}>
+            <boxGeometry />
+            <meshStandardMaterial color="red" />
           </mesh>
         </RigidBody>
       </Physics>

f89b129fd29fba42a992388e0f60868011f69193
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-19 11:16:08 +0800

    feat(66-create-a-game-with-r3f): 增加 BlockEnd 组件

--- 66-create-a-game-with-r3f/src/Level.jsx
+++ 66-create-a-game-with-r3f/src/Level.jsx
@@ -23,6 +23,19 @@ function BlockStart({ position = [0, 0, 0] }) {
     </group>
   );
 }
+function BlockEnd({ position = [0, 0, 0] }) {
+  return (
+    <group position={position}>
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, 0, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow={true}
+      ></mesh>
+    </group>
+  );
+}
 
 function BlockSpinner({ position = [0, 0, 0] }) {
   const obstacle = useRef();
@@ -159,10 +172,11 @@ function BlockAxe({ position = [0, 0, 0] }) {
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 12]} />
-      <BlockSpinner position={[0, 0, 8]} />
-      <BlockLimbo position={[0, 0, 4]} />
-      <BlockAxe position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 16]} />
+      <BlockSpinner position={[0, 0, 12]} />
+      <BlockLimbo position={[0, 0, 8]} />
+      <BlockAxe position={[0, 0, 4]} />
+      <BlockEnd position={[0, 0, 0]} />
     </>
   );
 }

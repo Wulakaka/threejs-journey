7e4d76c198ccceb3091446fb64caa305c742a2f4
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-20 17:59:24 +0800

    feat(66-create-a-game-with-r3f): 增加空格键监听

--- 66-create-a-game-with-r3f/src/Player.jsx
+++ 66-create-a-game-with-r3f/src/Player.jsx
@@ -1,11 +1,27 @@
 import { RigidBody } from "@react-three/rapier";
 import { useFrame } from "@react-three/fiber";
 import { useKeyboardControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Player() {
   const body = useRef();
   const [subscribeKeys, getKeys] = useKeyboardControls();
+
+  const jump = () => {
+    body.current.applyImpulse({ x: 0, y: 1, z: 0 });
+  };
+
+  useEffect(() => {
+    subscribeKeys(
+      (state) => state.jump,
+      (value) => {
+        if (value) {
+          jump();
+        }
+      },
+    );
+  }, []);
+
   useFrame((state, delta) => {
     const { forward, backward, leftward, rightward } = getKeys();
     const impulse = { x: 0, y: 0, z: 0 };

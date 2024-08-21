88dd2039be65feecf3dcab1d9001e2bd1ecf2dc5
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-21 15:45:44 +0800

    feat(66-create-a-game-with-r3f): 添加 Interface.jsx

--- 66-create-a-game-with-r3f/src/index.jsx
+++ 66-create-a-game-with-r3f/src/index.jsx
@@ -1,8 +1,9 @@
 import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
-import Experience from "./Experience.jsx";
 import { KeyboardControls } from "@react-three/drei";
+import Experience from "./Experience.jsx";
+import Interface from "./Interface.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -27,5 +28,6 @@ root.render(
     >
       <Experience />
     </Canvas>
+    <Interface />
   </KeyboardControls>,
 );

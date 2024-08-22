27d7ab2d0d31540884a76cdc5c654ca7138584b2
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-22 10:07:53 +0800

    feat(47-intro-and-loading-progress): 增加 loading bar

--- 47-intro-and-loading-progress/src/script.js
+++ 47-intro-and-loading-progress/src/script.js
@@ -6,12 +6,23 @@ import gsap from "gsap";
 /**
  * Loaders
  */
+const loadingBarElement = document.querySelector(".loading-bar");
 const loadingManager = new THREE.LoadingManager(
   () => {
-    gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0 });
+    window.setTimeout(() => {
+      gsap.to(overlayMaterial.uniforms.uAlpha, {
+        duration: 3,
+        value: 0,
+        delay: 1,
+      });
+
+      loadingBarElement.classList.add("ended");
+      loadingBarElement.style.transform = "";
+    }, 500);
   },
-  () => {
-    console.log("progress");
+  (itemUrl, itemsLoaded, itemsTotal) => {
+    const progressRatio = itemsLoaded / itemsTotal;
+    loadingBarElement.style.transform = `scaleX(${progressRatio})`;
   },
 );
 const gltfLoader = new GLTFLoader(loadingManager);

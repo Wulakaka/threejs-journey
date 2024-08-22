04db912298ff31b0c63c34831247e383fd7b9897
张伟峰 <zhang.weifeng@tslsmart.com>
2024-08-22 09:53:34 +0800

    feat(47-intro-and-loading-progress): 增加 overlay

--- 47-intro-and-loading-progress/src/script.js
+++ 47-intro-and-loading-progress/src/script.js
@@ -1,12 +1,21 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "gsap";
 
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
+const loadingManager = new THREE.LoadingManager(
+  () => {
+    gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0 });
+  },
+  () => {
+    console.log("progress");
+  },
+);
+const gltfLoader = new GLTFLoader(loadingManager);
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
 
 /**
  * Base
@@ -79,6 +88,31 @@ directionalLight.shadow.mapSize.set(1024, 1024);
 directionalLight.shadow.normalBias = 0.05;
 directionalLight.position.set(0.25, 3, -2.25);
 scene.add(directionalLight);
+
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+const overlayMaterial = new THREE.ShaderMaterial({
+  transparent: true,
+  uniforms: {
+    uAlpha: new THREE.Uniform(1),
+  },
+  vertexShader: `
+    void main() {
+      gl_Position = vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform float uAlpha;
+  
+    void main() {
+      gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+    }
+  `,
+});
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial);
+scene.add(overlay);
 
 /**
  * Sizes
